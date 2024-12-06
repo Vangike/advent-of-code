180c6a04c8035a2cc11b80979db792c3934c7c26
Vangike <vangmmike@gmail.com>
2024-12-06 13:22:11 -0600

    part2 setup

--- Days/Day2/day2.go
+++ Days/Day2/day2.go
@@ -2,7 +2,21 @@ package Day2
 
 import "fmt"
 
-func Run() {
-	fmt.Println("Day 2 logic!")
-	return
+func Run(part string) {
+	switch part {
+	case "1":
+		part1()
+	case "2":
+		part2()
+	default:
+		fmt.Printf("Part %s not implmented", part)
+	}
+}
+
+func part1() {
+
+}
+
+func part2() {
+	
 }
