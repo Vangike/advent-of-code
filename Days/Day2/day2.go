50628d3220e392881ab94bb2b0f469132421cd71
Vangike <vangmmike@gmail.com>
2024-12-06 20:06:47 -0600

    day2 initial logic

--- Days/Day2/day2.go
+++ Days/Day2/day2.go
@@ -1,6 +1,12 @@
 package Day2
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func Run(part string) {
 	switch part {
@@ -13,10 +19,58 @@ func Run(part string) {
 	}
 }
 
-func part1() {
+// ----------------
+// Supporting Functions
+// ----------------
 
+// Run input file
+func input() (*os.File) {
+	file, err := os.Open("Days/Day2/input.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return file
 }
 
+func twoDArray(file *os.File) ([][]int) {
+	array := [][]int{}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		items := strings.Fields(line)
+
+		valueArr := []int{}
+
+		for _, str := range items {
+			if value, err := strconv.Atoi(str); err == nil {
+				valueArr = append(valueArr, value)
+			}
+		}
+
+		array = append(array, valueArr)
+	}
+
+	return array
+}
+
+// ----------------
+// Part 1
+// ----------------
+func part1() {
+	file := input()
+	arrayValue := twoDArray(file)
+
+	fmt.Print(arrayValue)
+}
+
+// ----------------
+// Part 2
+// ----------------
 func part2() {
 	
 }
